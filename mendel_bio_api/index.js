31f0e540d029745377a0d38902387a09bccaafaf
marior <Canpci95.25>
2020-10-30 11:08:57 +0100

    auth_token as static

--- mendel_bio_api/index.js
+++ mendel_bio_api/index.js
@@ -128,7 +128,7 @@ class MendelBioApi extends MendelBase {
       return this.parent.post(`/minfo/${objectId}/results/all`, query)
     },
 
-    result(idResult) {
+    result(objectId,idResult) {
       return this.parent.get(`/minfo/${objectId}/results/${idResult}`)
     },
 
@@ -139,6 +139,29 @@ class MendelBioApi extends MendelBase {
     update(objectId, result) {
       return this.parent.put(`/minfo/${objectId}/results`, result)
     }
+
+  }
+
+  Person = {
+
+    all( query) {
+      return this.parent.post(`/person/all`, query)
+    },
+
+    allSamples(objectId ) {
+      return this.parent.get(`/person/${objectId}/samples`)
+    },
+
+    byId(objectId) {
+      return this.parent.get(`/person/${objectId}`)
+    },
+
+
+    update( person) {
+      return this.parent.put(`/person`, result)
+    }
+
+
 
   }
 
